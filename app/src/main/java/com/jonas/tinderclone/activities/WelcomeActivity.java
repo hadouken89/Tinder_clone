14c7feb45ce8c4bfce0d2eb84213cc839d8672a4
jonas_lopez <jonnas.88mig@gmail.com>
2019-08-26 16:42:50 -0300

    add WelcomeActivity login and register buttons

--- app/src/main/java/com/jonas/tinderclone/activities/WelcomeActivity.java
+++ app/src/main/java/com/jonas/tinderclone/activities/WelcomeActivity.java
@@ -1,13 +1,55 @@
 package com.jonas.tinderclone.activities;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class WelcomeActivity extends AppCompatActivity {
+import com.jonas.tinderclone.R;
+
+import butterknife.ButterKnife;
+import butterknife.InjectView;
+
+public class WelcomeActivity extends BaseActivity {
+
+    @InjectView(R.id.btnLogin)
+    Button btnLogin;
+    @InjectView(R.id.btnRegister)
+    Button btnRegister;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
+
+        ButterKnife.inject(this);
+
+        initProperties();
     }
+
+    @Override
+    public void initProperties() {
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(WelcomeActivity.this, LoginActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+
+        btnRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(WelcomeActivity.this, RegistrationActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+    }
+
+    @Override
+    public void configureActionBar() {}
+
+
 }
