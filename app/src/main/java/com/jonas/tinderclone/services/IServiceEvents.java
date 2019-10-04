5a5aca2068e9a2359313c75848fbe352303bb79a
jonas_lopez <jonnas.88mig@gmail.com>
2019-10-04 15:14:35 -0300

    implement ILoginServiceEvent on LoginService

---
+++ app/src/main/java/com/jonas/tinderclone/services/IServiceEvents.java
@@ -0,0 +1,8 @@
+package com.jonas.tinderclone.services;
+
+interface IServiceEvents {
+
+    void onStart();
+
+    void onStop();
+}
--- app/src/main/java/com/jonas/tinderclone/services/LoginService.java
+++ app/src/main/java/com/jonas/tinderclone/services/LoginService.java
@@ -2,32 +2,46 @@ package com.jonas.tinderclone.services;
 
 import android.app.Activity;
 import android.support.annotation.NonNull;
+
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
 
-public class LoginService {
+public class LoginService implements IServiceEvents {
 
     private Activity activity;
     private FirebaseAuth mAuth;
+    private DatabaseReference mDatabase;
 
     public LoginService(Activity activity) {
         this.activity = activity;
         this.mAuth = FirebaseAuth.getInstance();
     }
 
-    public void signInUser(String email, String password, final IResponseListener listener){
+    public void signInUser(final String email, String password, final IResponseListener listener) {
         mAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(activity, new OnCompleteListener<AuthResult>() {
             @Override
             public void onComplete(@NonNull Task<AuthResult> task) {
-                if(task.isSuccessful()){
+                if (task.isSuccessful()) {
                     listener.onSuccess();
-                }else {
+
+                } else {
                     String errorMessage = task.getException().getMessage();
                     listener.onFailure(errorMessage);
                 }
             }
         });
     }
+
+    @Override
+    public void onStart() {
+    }
+
+    @Override
+    public void onStop() {
+
+    }
+
 }
