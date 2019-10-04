aff5103c5c31f231d4b029523724d182d84d9bcc
jonas_lopez <jonnas.88mig@gmail.com>
2019-10-04 11:28:51 -0300

    add User class

---
+++ app/src/main/java/com/jonas/tinderclone/models/User.java
@@ -0,0 +1,41 @@
+package com.jonas.tinderclone.models;
+
+
+public class User {
+
+    private String name;
+    private String gender;
+
+    private UserCredentials userCredentials;
+
+    public User() { }
+
+    public User(String name, String gender){
+        this.name = name;
+        this.gender = gender;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public UserCredentials getUserCredentials() {
+        return userCredentials;
+    }
+
+    public void setUserCredentials(UserCredentials userCredentials) {
+        this.userCredentials = userCredentials;
+    }
+}
