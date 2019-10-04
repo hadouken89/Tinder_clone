6d4d9395d714e8c9e3226ca8053fad9ef9daa290
jonas_lopez <jonnas.88mig@gmail.com>
2019-10-04 15:11:49 -0300

    Write userData in firebase

--- app/src/main/java/com/jonas/tinderclone/services/RegisterService.java
+++ app/src/main/java/com/jonas/tinderclone/services/RegisterService.java
@@ -2,12 +2,13 @@ package com.jonas.tinderclone.services;
 
 import android.app.Activity;
 import android.support.annotation.NonNull;
-import android.widget.Toast;
-
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.jonas.tinderclone.models.User;
 
 public class RegisterService {
 
@@ -19,38 +20,31 @@ public class RegisterService {
         this.mAuth = FirebaseAuth.getInstance();
     }
 
-    public void registerUserEmailAccount(String email, String password, final IResponseListener listener ){
+    public void registerUserEmailAccount(final User userData, final IResponseListener listener) {
+
+        final String email = userData.getUserCredentials().getEmail();
+        final String password = userData.getUserCredentials().getPassword();
 
         mAuth.createUserWithEmailAndPassword(email, password)
                 .addOnCompleteListener(activity, new OnCompleteListener<AuthResult>() {
                     @Override
                     public void onComplete(@NonNull Task<AuthResult> task) {
-                        String toastMessage = task.isSuccessful() ? "Successful registration!" : "Error in registration";
+                        if (task.isSuccessful()) {
 
-                        Toast.makeText(activity, toastMessage, Toast.LENGTH_LONG);
-                        if(task.isSuccessful() ){
+                            DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReference("Users");
+                            String id = databaseReference.push().getKey();
+                            databaseReference.child(id).setValue(userData);
+
                             listener.onSuccess();
-                        }
-                        else{
+
+                        } else {
                             String errorMessage = task.getException().getMessage();
                             listener.onFailure(errorMessage);
                         }
-
                     }
+
                 });
 
-    //    mAuth.addAuthStateListener(new FirebaseAuth.AuthStateListener() {
-    //        @Override
-    //        public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
-    //            final FirebaseUser user = firebaseAuth.getInstance().getCurrentUser();
-    //            if(user != null )
-    //                listener.onSuccess();
-    //            else
-    //                listener.onFailure();
-    //        }
-    //    });
-
     }
-
 
 }
