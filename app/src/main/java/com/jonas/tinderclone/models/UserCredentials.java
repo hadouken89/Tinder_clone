7dca91b90141ee93648be5a25afa50a22f8a628f
jonas_lopez <jonnas.88mig@gmail.com>
2019-10-04 11:29:05 -0300

    add UserCredentials class

---
+++ app/src/main/java/com/jonas/tinderclone/models/UserCredentials.java
@@ -0,0 +1,31 @@
+package com.jonas.tinderclone.models;
+
+public class UserCredentials {
+
+    private String password;
+    private String email;
+
+    public UserCredentials() {
+    }
+
+    public UserCredentials(String password, String email) {
+        this.password = password;
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
